9d85b790c2fe4e29a5c149efded6e42cd221bcd6
hengamevalizade123 <valizadehengame@yahoo.com>
2025-08-28 12:24:32 +0330

    added:statistics section

---
+++ template-parts/partials/home/statistics.php
@@ -0,0 +1,40 @@
+<section class="o-section c-section--statistics">
+	<div class="o-section__wrapper">
+		<div class="c-statistics">
+			<ul class="p-0 m-0 d-flex">
+				<li class="d-flex flex-column items-center">
+					<strong>
+						۲/۳۷۰
+					</strong>
+					<span>
+تعداد مشاوره‌ها
+		</span>
+				</li>
+				<li class="d-flex flex-column items-center">
+					<strong>
+						۳۳۲/۰۰۰
+					</strong>
+					<span>
+تعداد فالوور اینستاگرام
+		</span>
+				</li>
+				<li class="d-flex flex-column items-center">
+					<strong>
+						۳۷۹
+					</strong>
+					<span>
+تعداد ویدئوهای آموزشی
+		</span>
+				</li>
+				<li class="d-flex flex-column items-center">
+					<strong>
+						۱/۲۰۵
+					</strong>
+					<span>
+تعداد کاربران آنلاین
+		</span>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
